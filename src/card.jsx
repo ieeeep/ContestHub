ffe003d2330d944ad0018fdd406d8a660557e0cf
Ria <ieeprty456@gmail.com>
2025-12-07 18:48:39 +0300

    Remake cards in swiper

--- src/card.jsx
+++ src/card.jsx
@@ -13,11 +13,16 @@ export default function Card({ image, title, text, isCentral }) {
     //     <p>{text && text.length > 30 ? `${text.slice(0,40)}...` : text}</p>
     //   </div>
     // </div>
+    // <div className='card'>
+      // <img src={image} className='poster'></img>
+      // <div className='infoText'>
+        // <h1>{title}</h1>
+        // <p>{text && text.length > 100 ? `${text.slice(0,100)}...` : text}</p>
+      // </div>
     <div className='card'>
-      <img src={image} className='poster'></img>
+      <img src={image} className='poster' alt={title} />
       <div className='infoText'>
         <h1>{title}</h1>
-        <p>{text && text.length > 100 ? `${text.slice(0,100)}...` : text}</p>
       </div>
     </div>
   )
